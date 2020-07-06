dd3951aaf8a4a821e508377422ee7e730f28a6e1
Terry Wang <wtyanan@google.com>
2020-07-06 19:20:24 +0000

    Demo folder

---
+++ zPageMaven/src/main/java/com/mycompany/otelexample/App.java
@@ -0,0 +1,82 @@
+package com.mycompany.otelexample;
+
+// import io.opentelemetry.common.Attributes;
+import io.opentelemetry.common.AttributeValue;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.exporters.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.extensions.zpages.ZPageServer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.trace.Span;
+import io.opentelemetry.trace.Tracer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.io.IOException;
+
+public class App
+{
+  private static final Logger logger =
+      Logger.getLogger(App.class.getName());
+  private static LoggingSpanExporter loggingExporter = new LoggingSpanExporter();
+
+  public static void main( String[] args )
+  {
+    try {
+      ZPageServer.startHttpServerAndRegisterAllPages(8080);
+    }
+    catch (IOException e) {
+    }
+    // Configure the LoggingExporter as our exporter.
+    SpanProcessor spanProcessor =
+        SimpleSpanProcessor.newBuilder(loggingExporter).build();
+    OpenTelemetrySdk.getTracerProvider().addSpanProcessor(spanProcessor);
+
+    // Add a single Span.
+    Tracer tracer =
+        OpenTelemetrySdk.getTracerProvider().get("otelexample");
+    Span span = tracer.spanBuilder("foo").startSpan();
+    // span.addEvent("operation.request_started", Attributes.of("id", AttributeValue.stringAttributeValue("XYZ")));
+    span.addEvent("operation.request_started");
+    span.setAttribute("operation.id", 7);
+    span.setAttribute("operation.name", "app");
+
+    // Set it as the current Span.
+    try (Scope scope = tracer.withSpan(span)) {
+
+      // Add another Span, as an implicit child.
+      Span childSpan = tracer.spanBuilder("bar").startSpan();
+      childSpan.addEvent("operation.request_started");
+      childSpan.setAttribute("operation.id", 9);
+
+      try (Scope childScope = tracer.withSpan(childSpan)) {
+        logger.info("Active Span: " + tracer.getCurrentSpan());
+      } finally {
+        childSpan.end();
+      }
+    }
+
+    span = tracer.spanBuilder("foo").startSpan();
+    span.setAttribute("operation.id", 7);
+    span.addEvent("operation.request_started");
+
+    // Set it as the current Span.
+    try (Scope scope = tracer.withSpan(span)) {
+
+      // Add another Span, as an implicit child.
+      Span childSpan = tracer.spanBuilder("bar").startSpan();
+      childSpan.setAttribute("operation.id", 9);
+      childSpan.addEvent("operation.request_started");
+
+      try (Scope childScope = tracer.withSpan(childSpan)) {
+        logger.info("Active Span: " + tracer.getCurrentSpan());
+      } finally {
+        childSpan.end();
+      }
+    }
+
+    spanProcessor.shutdown();
+  }
+}
