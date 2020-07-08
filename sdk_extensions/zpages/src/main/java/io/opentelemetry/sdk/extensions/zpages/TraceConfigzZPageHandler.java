b3b181474240d3e24134ef877455766d678103c8
Terry Wang <wtyanan@google.com>
2020-07-08 19:50:41 +0000

    Implemented active parameter value table

--- sdk_extensions/zpages/src/main/java/io/opentelemetry/sdk/extensions/zpages/TraceConfigzZPageHandler.java
+++ sdk_extensions/zpages/src/main/java/io/opentelemetry/sdk/extensions/zpages/TraceConfigzZPageHandler.java
@@ -20,8 +20,6 @@ import io.opentelemetry.sdk.trace.config.TraceConfig;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
-import java.util.Formatter;
-import java.util.Locale;
 import java.util.Map;
 
 final class TraceConfigzZPageHandler extends ZPageHandler {
@@ -36,8 +34,201 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
       "maxnumofattributesperlink";
   // Background color used for zebra striping rows in table
   private static final String ZEBRA_STRIPE_COLOR = "#e6e6e6";
+  private final TraceConfig traceConfig;
 
-  TraceConfigzZPageHandler() {}
+  /** Builder pattern class for emiting a single row of the change parameter table. */
+  private static final class ChangeTableRow {
+    private final PrintStream out;
+    private final String rowName;
+    private final String paramName;
+    private final String defaultValue;
+    private final boolean zebraStripe;
+
+    private ChangeTableRow(Builder builder) {
+      out = builder.out;
+      rowName = builder.rowName;
+      paramName = builder.paramName;
+      defaultValue = builder.defaultValue;
+      zebraStripe = builder.zebraStripe;
+    }
+
+    public static class Builder {
+      private PrintStream out;
+      private String rowName;
+      private String paramName;
+      private String defaultValue;
+      private boolean zebraStripe;
+
+      public ChangeTableRow build() {
+        return new ChangeTableRow(this);
+      }
+
+      /**
+       * Set the print stream to emit HTML contents.
+       *
+       * @param out the {@link PrintStream} {@code out}.
+       * @return the {@link Builder}.
+       */
+      public Builder setPrintStream(PrintStream out) {
+        this.out = out;
+        return this;
+      }
+
+      /**
+       * Set the display name of the parameter the row corresponds to.
+       *
+       * @param rowName the display name of the parameter the row corresponds to.
+       * @return the {@link Builder}.
+       */
+      public Builder setRowName(String rowName) {
+        this.rowName = rowName;
+        return this;
+      }
+
+      /**
+       * Set the parameter name the row corresponds to.
+       *
+       * @param paramName the parameter name the row corresponds to (this will be used as URL query
+       *     parameter, e.g. /traceconfigz?maxnumofattributes=30).
+       * @return the {@link Builder}.
+       */
+      public Builder setParamName(String paramName) {
+        this.paramName = paramName;
+        return this;
+      }
+
+      /**
+       * Set the default value of the parameter the row corresponds to.
+       *
+       * @param defaultValue the default value of the corresponding parameter.
+       * @return the {@link Builder}.
+       */
+      public Builder setParamDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+        return this;
+      }
+
+      /**
+       * Set the boolean for zebraStriping the row.
+       *
+       * @param zebraStripe the boolean for zebraStriping the row.
+       * @return the {@link Builder}.
+       */
+      public Builder setZebraStripe(boolean zebraStripe) {
+        this.zebraStripe = zebraStripe;
+        return this;
+      }
+    }
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    /** Emit HTML content to the PrintStream. */
+    public void emitHtml() {
+      if (zebraStripe) {
+        out.print("<tr style=\"background-color: " + ZEBRA_STRIPE_COLOR + ";\">");
+      } else {
+        out.print("<tr>");
+      }
+      out.print("<td>" + rowName + "</td>");
+      out.print(
+          "<td class=\"border-left-dark\"><input type=text size=15 name="
+              + paramName
+              + " value=\"\" /></td>");
+      out.print("<td class=\"border-left-dark\">(" + defaultValue + ")</td>");
+      out.print("</tr>");
+    }
+  }
+
+  private static final class ActiveTableRow {
+    private final PrintStream out;
+    private final String paramName;
+    private final String paramValue;
+    private final boolean zebraStripe;
+
+    private ActiveTableRow(Builder builder) {
+      out = builder.out;
+      paramName = builder.paramName;
+      paramValue = builder.paramValue;
+      zebraStripe = builder.zebraStripe;
+    }
+
+    public static class Builder {
+      private PrintStream out;
+      private String paramName;
+      private String paramValue;
+      private boolean zebraStripe;
+
+      public ActiveTableRow build() {
+        return new ActiveTableRow(this);
+      }
+
+      /**
+       * Set the print stream to emit HTML contents.
+       *
+       * @param out the {@link PrintStream} {@code out}.
+       * @return the {@link Builder}.
+       */
+      public Builder setPrintStream(PrintStream out) {
+        this.out = out;
+        return this;
+      }
+
+      /**
+       * Set the parameter name the row corresponds to.
+       *
+       * @param paramName the parameter name the row corresponds to.
+       * @return the {@link Builder}.
+       */
+      public Builder setParamName(String paramName) {
+        this.paramName = paramName;
+        return this;
+      }
+
+      /**
+       * Set the parameter value the row corresponds to.
+       *
+       * @param paramValue the parameter value the row corresponds to.
+       * @return the {@link Builder}.
+       */
+      public Builder setParamValue(String paramValue) {
+        this.paramValue = paramValue;
+        return this;
+      }
+
+      /**
+       * Set the boolean for zebraStriping the row.
+       *
+       * @param zebraStripe the boolean for zebraStriping the row.
+       * @return the {@link Builder}.
+       */
+      public Builder setZebraStripe(boolean zebraStripe) {
+        this.zebraStripe = zebraStripe;
+        return this;
+      }
+    }
+
+    public static Builder builder() {
+      return new Builder();
+    }
+
+    /** Emit HTML content to the PrintStream. */
+    public void emitHtml() {
+      if (zebraStripe) {
+        out.print("<tr style=\"background-color: " + ZEBRA_STRIPE_COLOR + ";\">");
+      } else {
+        out.print("<tr>");
+      }
+      out.print("<td>" + paramName + "</td>");
+      out.print("<td class=\"border-left-dark\">" + paramValue + "</td>");
+      out.print("</tr>");
+    }
+  }
+
+  TraceConfigzZPageHandler(TraceConfig traceConfig) {
+    this.traceConfig = traceConfig;
+  }
 
   @Override
   public String getUrlPath() {
@@ -56,145 +247,29 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
     out.print("</style>");
   }
 
-  /** Builder pattern class for emiting a single row of the change parameter table. */
-  private static final class ChangeTableRow {
-    private final PrintStream out;
-    private final Formatter formatter;
-    private final String rowName;
-    private final String paramName;
-    private final String defaultValue;
-    private final boolean zebraStripe;
-
-    private ChangeTableRow(Builder builder) {
-      out = builder.out;
-      formatter = builder.formatter;
-      rowName = builder.rowName;
-      paramName = builder.paramName;
-      defaultValue = builder.defaultValue;
-      zebraStripe = builder.zebraStripe;
-    }
-
-    public static class Builder {
-      public ChangeTableRow build() {
-        return new ChangeTableRow(this);
-      }
-
-      /**
-       * Set the print stream to emit HTML contents.
-       *
-       * @param out the {@link PrintStream} {@code out}.
-       * @return the {@link Builder}.
-       */
-      public Builder setPrintStream(PrintStream out) {
-        this.out = out;
-        return this;
-      }
-
-      /**
-       * Set the print stream to emit HTML contents.
-       *
-       * @param formatter a {@link Formatter} for formatting HTML expressions.
-       * @return the {@link Builder}.
-       */
-      public Builder setFormatter(Formatter formatter) {
-        this.formatter = formatter;
-        return this;
-      }
-
-      /**
-       * Set the print stream to emit HTML contents.
-       *
-       * @param rowName the display name of the parameter the row.
-       * @return the {@link Builder}.
-       */
-      public Builder setRowName(String rowName) {
-        this.rowName = rowName;
-        return this;
-      }
-
-      /**
-       * Set the print stream to emit HTML contents.
-       *
-       * @param paramName the parameter name the row corresponds to (this will be used as URL query
-       *     parameter, e.g. /traceconfigz?maxnumofattributes=30).
-       * @return the {@link Builder}.
-       */
-      public Builder setParamName(String paramName) {
-        this.paramName = paramName;
-        return this;
-      }
-
-      /**
-       * Set the print stream to emit HTML contents.
-       *
-       * @param defaultValue the default value of the corresponding parameter.
-       * @return the {@link Builder}.
-       */
-      public Builder setParamDefaultValue(String defaultValue) {
-        this.defaultValue = defaultValue;
-        return this;
-      }
-
-      /**
-       * Set the print stream to emit HTML contents.
-       *
-       * @param zebraStripe the boolean for zebraStriping rows.
-       * @return the {@link Builder}.
-       */
-      public Builder setZebraStripe(boolean zebraStripe) {
-        this.zebraStripe = zebraStripe;
-        return this;
-      }
-
-      private PrintStream out;
-      private Formatter formatter;
-      private String rowName;
-      private String paramName;
-      private String defaultValue;
-      private boolean zebraStripe;
-    }
-
-    public static Builder builder() {
-      return new Builder();
-    }
-
-    public void emitHtml() {
-      if (zebraStripe) {
-        formatter.format("<tr style=\"background-color: %s;\">", ZEBRA_STRIPE_COLOR);
-      } else {
-        out.print("<tr>");
-      }
-      formatter.format("<td>%s</td>", rowName);
-      formatter.format(
-          "<td class=\"border-left-dark\"><input type=text size=15 name=%s value=\"\" /></td>",
-          paramName);
-      formatter.format("<td class=\"border-left-dark\">(%s)</td>", defaultValue);
-      out.print("</tr>");
-    }
-  }
-
   /**
    * Emits the change tracing parameter table to the {@link PrintStream} {@code out}.
    *
    * @param out the {@link PrintStream} {@code out}.
-   * @param formatter a {@link Formatter} for formatting HTML expressions.
    */
-  private static void emitChangeTable(PrintStream out, Formatter formatter) {
+  private static void emitChangeTable(PrintStream out) {
     out.print("<table style=\"border-spacing: 0; border: 1px solid #363636;\">");
     out.print("<tr class=\"bg-color\">");
-    out.print("<th colspan=3 class=\"header-text\"><b>Permanently change</b></th>");
+    out.print(
+        "<th colspan=2 style=\"text-align: left;\" class=\"header-text\">"
+            + "<b>Permanently change</b></th>");
+    out.print(
+        "<th colspan=1 class=\"header-text border-left-white\"><b>Permanently change</b></th>");
     ChangeTableRow.builder()
         .setPrintStream(out)
-        .setFormatter(formatter)
         .setRowName("SamplingProbability to")
         .setParamName(QUERY_STRING_SAMPLING_PROBABILITY)
-        .setParamDefaultValue("1.0")
+        .setParamDefaultValue(TraceConfig.getDefault().getSampler().getDescription())
         .setZebraStripe(false)
         .build()
         .emitHtml();
     ChangeTableRow.builder()
         .setPrintStream(out)
-        .setFormatter(formatter)
         .setRowName("MaxNumberOfAttributes to")
         .setParamName(QUERY_STRING_MAX_NUM_OF_ATTRIBUTES)
         .setParamDefaultValue(Integer.toString(TraceConfig.getDefault().getMaxNumberOfAttributes()))
@@ -203,7 +278,6 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
         .emitHtml();
     ChangeTableRow.builder()
         .setPrintStream(out)
-        .setFormatter(formatter)
         .setRowName("MaxNumberOfEvents to")
         .setParamName(QUERY_STRING_MAX_NUM_OF_EVENTS)
         .setParamDefaultValue(Integer.toString(TraceConfig.getDefault().getMaxNumberOfEvents()))
@@ -212,7 +286,6 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
         .emitHtml();
     ChangeTableRow.builder()
         .setPrintStream(out)
-        .setFormatter(formatter)
         .setRowName("MaxNumberOfLinks to")
         .setParamName(QUERY_STRING_MAX_NUM_OF_LINKS)
         .setParamDefaultValue(Integer.toString(TraceConfig.getDefault().getMaxNumberOfLinks()))
@@ -221,7 +294,6 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
         .emitHtml();
     ChangeTableRow.builder()
         .setPrintStream(out)
-        .setFormatter(formatter)
         .setRowName("MaxNumberOfAttributesPerEvent to")
         .setParamName(QUERY_STRING_MAX_NUM_OF_ATTRIBUTES_PER_EVENT)
         .setParamDefaultValue(
@@ -231,7 +303,6 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
         .emitHtml();
     ChangeTableRow.builder()
         .setPrintStream(out)
-        .setFormatter(formatter)
         .setRowName("MaxNumberOfAttributesPerLink to")
         .setParamName(QUERY_STRING_MAX_NUM_OF_ATTRIBUTES_PER_LINK)
         .setParamDefaultValue(
@@ -239,17 +310,69 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
         .setZebraStripe(true)
         .build()
         .emitHtml();
+    out.print("</table>");
   }
 
   /**
    * Emits the active tracing parameters table to the {@link PrintStream} {@code out}.
    *
    * @param out the {@link PrintStream} {@code out}.
-   * @param formatter a {@link Formatter} for formatting HTML expressions.
    */
-  private static void emitActiveTable(PrintStream out, Formatter formatter) {
-    out.print("");
-    formatter.format("");
+  private void emitActiveTable(PrintStream out) {
+    out.print("<table style=\"border-spacing: 0; border: 1px solid #363636;\">");
+    out.print("<tr class=\"bg-color\">");
+    out.print("<th class=\"header-text\"><b>Name</b></th>");
+    out.print("<th class=\"header-text border-left-white\"><b>Value</b></th>");
+    out.print("</tr>");
+    ActiveTableRow.builder()
+        .setPrintStream(out)
+        .setParamName("Sampler")
+        .setParamValue(traceConfig.getSampler().getDescription())
+        .setZebraStripe(false)
+        .build()
+        .emitHtml();
+    ;
+    ActiveTableRow.builder()
+        .setPrintStream(out)
+        .setParamName("MaxNumOfAttributes")
+        .setParamValue(Integer.toString(traceConfig.getMaxNumberOfAttributes()))
+        .setZebraStripe(false)
+        .build()
+        .emitHtml();
+    ;
+    ActiveTableRow.builder()
+        .setPrintStream(out)
+        .setParamName("MaxNumOfEvents")
+        .setParamValue(Integer.toString(traceConfig.getMaxNumberOfEvents()))
+        .setZebraStripe(false)
+        .build()
+        .emitHtml();
+    ;
+    ActiveTableRow.builder()
+        .setPrintStream(out)
+        .setParamName("MaxNumOfLinks")
+        .setParamValue(Integer.toString(traceConfig.getMaxNumberOfLinks()))
+        .setZebraStripe(false)
+        .build()
+        .emitHtml();
+    ;
+    ActiveTableRow.builder()
+        .setPrintStream(out)
+        .setParamName("MaxNumOfAttributesPerEvent")
+        .setParamValue(Integer.toString(traceConfig.getMaxNumberOfAttributesPerEvent()))
+        .setZebraStripe(false)
+        .build()
+        .emitHtml();
+    ;
+    ActiveTableRow.builder()
+        .setPrintStream(out)
+        .setParamName("MaxNumOfAttributesPerLink")
+        .setParamValue(Integer.toString(traceConfig.getMaxNumberOfAttributesPerLink()))
+        .setZebraStripe(false)
+        .build()
+        .emitHtml();
+    ;
+    out.print("</table>");
   }
 
   /**
@@ -259,17 +382,16 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
    * @param queryMap the map containing URL query parameters
    * @param out the {@link PrintStream} {@code out}.
    */
-  private static void emitHtmlBody(Map<String, String> queryMap, PrintStream out)
+  private void emitHtmlBody(Map<String, String> queryMap, PrintStream out)
       throws UnsupportedEncodingException {
     out.print(
         "<img style=\"height: 90px;\" src=\"data:image/png;base64,"
             + ZPageLogo.logoBase64
             + "\" />");
     out.print("<h1>Trace Configuration</h1>");
-    Formatter formatter = new Formatter(out, Locale.US);
     out.print("<form class=\"form-flex\" action=\"" + TRACE_CONFIGZ_URL + "\" method=\"get\">");
     out.print("<input type=\"hidden\" name=\"change\" value=\"\" />");
-    emitChangeTable(out, formatter);
+    emitChangeTable(out);
     // Button for submit
     out.print("<button class=\"button\" type=\"submit\" value=\"Submit\">Submit</button>");
     out.print("</form>");
@@ -278,7 +400,7 @@ final class TraceConfigzZPageHandler extends ZPageHandler {
     out.print("<input type=\"hidden\" name=\"default\" value=\"\" />");
     out.print("<button class=\"button\" type=\"submit\" value=\"Submit\">Restore Default</button>");
     out.print("</form>");
-    emitActiveTable(out, formatter);
+    emitActiveTable(out);
     // deal with query map
     queryMap.toString();
   }
